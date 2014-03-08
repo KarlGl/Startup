f3243e65eebdf257022b514b50af7ac9e0b74624
Karl Glaser <kg@papercloud.com.au>
2014-03-08 18:02:16 +1100

    some skeliton commented out

---
+++ js/frontend.js
@@ -0,0 +1,20 @@
+// var app = Ember.Application.create()
+
+// // Router
+// app.Router.map(function() {
+//   this.route('foo');
+// });
+
+// // Routes 
+// app.IndexRoute = Ember.Route.extend({
+// })
+
+// // Controllers
+// app.IndexController = Ember.Controller.extend({
+// });
+
+// // Views
+// app.IndexView = Ember.View.extend({
+// })
+
+// window.App = app;
