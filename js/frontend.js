f7e149959f1b44293127253adeec68f482aecf1e
Karl Glaser <kg@papercloud.com.au>
2014-03-09 13:05:09 +1100

    generate btn takes you to idea route

--- js/frontend.js
+++ js/frontend.js
@@ -1,20 +1,38 @@
-// var app = Ember.Application.create()
+var app = Ember.Application.create()
 
-// // Router
-// app.Router.map(function() {
-//   // this.route('idea', );
-// });
+// Router
+app.Router.map(function() {
+    this.route('idea', {
+        path: 'idea/:idea_string'
+    });
+});
 
-// // Routes 
-// app.IndexRoute = Ember.Route.extend({
-// })
+// Routes 
+app.ApplicationRoute = Ember.Route.extend({})
+app.IdeaRoute = Ember.Route.extend({
+    setupController: function(controller, model) {
+        controller.set('ideaString', model.idea_string);
+    }
+})
 
-// // Controllers
-// app.IndexController = Ember.Controller.extend({
-// });
+// Controllers
+app.ApplicationController = Ember.Controller.extend({
+	actions: {
+		generate: function() {
+			this.transitionToRoute('idea', 'test');
+		}
+	}
+})
+app.IdeaController = Ember.Controller.extend({})
 
-// // Views
-// app.IndexView = Ember.View.extend({
-// })
+// Views
+app.ApplicationView = Ember.View.extend({})
+app.IdeaView = Ember.View.extend({
+	templateName: 'idea',
 
-// window.App = app;
+	// The element is in the DOM after this function.
+	didInsertElement: function() {
+	}
+})
+
+window.App = app;
