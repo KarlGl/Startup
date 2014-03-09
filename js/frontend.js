d699ffbc0b1f60968432310b8d9fdbe3f7f4bcb2
Karl Glaser <kg@papercloud.com.au>
2014-03-09 13:30:34 +1100

    seeds skeliton

--- js/frontend.js
+++ js/frontend.js
@@ -5,34 +5,41 @@ app.Router.map(function() {
     this.route('idea', {
         path: 'idea/:idea_string'
     });
+    this.route('seeds');
 });
 
 // Routes 
 app.ApplicationRoute = Ember.Route.extend({});
 app.IdeaRoute = Ember.Route.extend({
-		model: function(params) {
-			return params.idea_string;
-		}
+    model: function(params) {
+        return params.idea_string;
+    }
 })
+app.SeedsRoute = Ember.Route.extend({
+    model: function() {
+        return window.genrationSeeds;
+    }
+});
 
 // Controllers
 app.ApplicationController = Ember.Controller.extend({
-	actions: {
-		generate: function() {
-			this.transitionToRoute('idea', runGenerator().join(" "));
-		}
-	}
+    actions: {
+        generate: function() {
+            this.transitionToRoute('idea',
+                runGenerator().join(" "));
+        }
+    }
 })
 app.IdeaController = Ember.Controller.extend({});
+app.SeedsController = Ember.Controller.extend({});
 
 // Views
 app.ApplicationView = Ember.View.extend({});
 app.IdeaView = Ember.View.extend({
-	templateName: 'idea',
+    templateName: 'idea',
 
-	// The element is in the DOM after this function.
-	didInsertElement: function() {
-	}
+    // The element is in the DOM after this function.
+    didInsertElement: function() {}
 });
 
 window.App = app;
